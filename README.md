7f0398232b5fc0439381a248270fabb9be230986
Wallacethewonderer <123340192+Wallacethewonderer@users.noreply.github.com>
2023-04-10 18:26:48 -0700

    Readme_updated

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# GenRe4dMe
-README File Generator
+# GenRe4me
+
+![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://choosealicense.com/licenses)
+
+## Description
+
+A README File Generator
+
+## Table of Contents
+
+- [Installation](##installation)
+- [Usage](##usage)
+- [Contributing](##contributing)
+- [Tests](##tests)
+- [License](##license)
+- [Questions](##questions)
+
+## Installation
+
+1.Clone the repo. 2.run the command "npm i inquirer@8.2.4" in the terminal after cd into the repo's directory. 3.run "node index.js". 3.Fill in the info. 4.Finally, a README file will be created.
+
+## Usage
+
+To use the generator, just follow 3-5 steps of installation instructions.
+
+## Contributing
+
+It's open for everyone.
+
+## Tests
+
+Basically, just run it in the termianl.
+
+## License
+
+This project is licensed under the MIT license. Click the badge at the top of the README to learn more about the license terms and conditions.
+
+## Questions
+
+If you have any questions about this project, please contact me via:
+
+- Email: yuhe.liang@outlook.com
+- GitHub: https://github.com/Wallacethewonderer
--- index.js
+++ index.js
@@ -1,13 +1,75 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        name: 'title',
+        message: 'What is the title of your project?',
+        type: 'input'
+    },
+    {
+        name: 'description',
+        message: 'Please provide a description of for project.',
+        type: 'input'
+    },
+    {
+        name: 'installation',
+        message: 'Please provide installation instructions for your project.',
+        type: 'input'
+    },
+    {
+        name: 'usage',
+        message: 'Please provide usage information for your project.',
+        type: 'input'
+    },
+    {
+        name: 'contribution',
+        message: 'Please provide contribution guidelines for your project.',
+        type: 'input'
+    },
+    {
+        name: 'test',
+        message: 'Please provide test instructions for your project.',
+        type: 'input'
+    },
+    {
+        name: 'license',
+        message: 'Please select a license for your project.',
+        type: 'list',
+        choices: ['MIT', 'Apache', 'GPL', 'BSD', 'None']
+    },
+    {
+        name: 'github',
+        message: 'Please provide your GitHub username.',
+        type: 'input'
+    },
+    {
+        name: 'email',
+        message: 'Please provide your email address.',
+        type: 'input'
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeToFile(fileName, data, (err) => {
+        if (err) {
+            throw err;
+        }
+        console.log(`${fileName} is created successfully!`);
+    }
+)}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((data) => {
+        const markdown = generateMarkdown(data);
+        writeToFile('README.md', generateMarkdown(data));
+    });
+}
 
 // Function call to initialize app
 init();
